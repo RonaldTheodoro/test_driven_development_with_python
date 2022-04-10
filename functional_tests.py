3302b0f11351e7920b831ecc3372637936fa1a6e
Ronald Theodoro <ronaldtheodoro@gmail.com>
2022-04-10 19:30:23 -0300

    Functional test now check we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,8 @@
+import time
 import unittest
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVistorTest(unittest.TestCase):
@@ -19,20 +21,35 @@ class NewVistorTest(unittest.TestCase):
         # Ela percebe que o título da página e o cabeçalho mencionam
         # listas de tarefas (to-do)
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # Ela é convidada a inserir um item de tarefa imediatamente
+        inputbox = self.browser.find_element_by_tag_name('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # Ela digita "Buy peacock feathers" (comprar penas de pavão) em uma
         # caixa de texto (o hobby de Edith é fazer iscas para pesca com fly)
-
+        inputbox.send_keys('Buy peacock feathers')
 
         # Quando ela tecla enter, a pagina é atualizada, e agora a página lista
         # "1: Buy peacock feathers" como um item em uma lista de tarefas
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
 
         # Ainda continua havendo uma caixa de texto convidando-a a acrecentar
         # outro item. Ela insere "Use peacock feathers to make a fly"
         # (Usar penas de pavão para fazer um fly - Edith é bem metódica)
+        self.fail('Finish the test!')
 
         # A página é atualizada novamente e agora mostra os dois items
         # em sua lista
