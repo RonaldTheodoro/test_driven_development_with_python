28051ad020b625d0234455286408c6a937b947df
Ronald Theodoro <ronaldtheodoro@gmail.com>
2022-04-10 19:54:37 -0300

    Refactor index view to usa a template

--- apps/lists/views.py
+++ apps/lists/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('<html><title>To-Do lists</title></html>')
+    return render(request, 'index.html')
+
--- superlists/settings.py
+++ superlists/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Project apps
+    'apps.lists',
 ]
 
 MIDDLEWARE = [
