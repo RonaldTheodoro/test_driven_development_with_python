9c0b1f4cb12189fe1dd650c5ba38d151c14aacaf
Ronald Theodoro <ronaldtheodoro@gmail.com>
2022-04-12 20:56:03 -0300

    Show text in the table

--- apps/lists/views.py
+++ apps/lists/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')
+    context = {'new_item_text': request.POST.get('item_text', '')}
+    return render(request, 'index.html', context)
 
--- functional_tests.py
+++ functional_tests.py
@@ -42,22 +42,30 @@ class NewVistorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            'New to-do item did not apper in table'
-        )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # Ainda continua havendo uma caixa de texto convidando-a a acrecentar
         # outro item. Ela insere "Use peacock feathers to make a fly"
         # (Usar penas de pavão para fazer um fly - Edith é bem metódica)
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # A página é atualizada novamente e agora mostra os dois items
         # em sua lista
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly',
+            [row.text for row in rows]
+        )
 
         # Edith se pergunta se o site lembrará de sua lista. Então ela nota
         # que o site gerou um URL único para ela -- há um pequeno
         # texto explicativo para isso.
+        self.fail('Finish the test!')
 
         # Ela acessa esse URL - sua lista de tarefas continua lá.
 
