1bdf0a039bfe0e0f10666c826178914e4373f3f9
Ronald Theodoro <ronaldtheodoro@gmail.com>
2022-04-16 22:54:12 -0300

    first steps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -114,4 +114,27 @@ class NewVistorTest(LiveServerTestCase):
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
 
+    def test_layout_and_styling(self):
+        # Edith acessa a página inicial
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
 
+        # Ela percebe que a caixa de entrada está elegentemente centralizada
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+        # Ela inicia uma nova lista e vê que a entrada está elegantemente
+        # centralizada aí também
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
